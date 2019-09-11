5fe966bcc784bd854baca9429e20da55ce4c00d7
Donovan.Xu <d.east6@gmail.com>
2019-09-11 17:26:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <notification></notification>
+    <router-view />
+  </div>
+
+</template>
+<script>
+  import Notification from './components/common/Notification'
+  import JwtToken from './helpers/jwt'
+
+  export default {
+    created(){
+      if (JwtToken.getToken()) {
+        this.$store.dispatch('setAuthUser')
+      }else if (JwtToken.getAuthId()){
+        this.$store.dispatch('refreshToken')
+      }
+    },
+    components:{
+      Notification
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+
+import zh_CN from './locale/zh_CN'
+import * as VeeValidate from 'vee-validate'
+
+Vue.use(VeeValidate);
+VeeValidate.localize('zh_CN', zh_CN);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
