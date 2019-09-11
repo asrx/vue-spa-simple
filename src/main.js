a6a6a7a38075f0d08ab0606d3d857813160561bb
Donovan.Xu <d.east6@gmail.com>
2019-09-11 17:22:39 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
-import JwtToken from './helpers/jwt'
 
 import zh_CN from './locale/zh_CN'
 import * as VeeValidate from 'vee-validate'
